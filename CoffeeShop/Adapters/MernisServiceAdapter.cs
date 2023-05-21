ea4e2fc55cee3d1afeedec667caea9b790c69e3a
Emin Tatar <emintatar48@outlook.com>
2023-05-21 17:15:07 +0300

    first commit

---
+++ CoffeeShop/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Abstract;
+using CoffeeShop.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            Task<TCKimlikNoDogrulaResponse> tcKimlikTask = client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.LastName, customer.DateOfBirth.Year);
+            return tcKimlikTask.Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ CoffeeShop/Program.cs
@@ -0,0 +1,24 @@
+﻿using CoffeeShop.Abstract;
+using CoffeeShop.Adapters;
+using CoffeeShop.Concrete;
+using CoffeeShop.Entities;
+using System;
+
+namespace CoffeeShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Abdullah Emin";
+            customer.LastName = "Tatar";
+            customer.DateOfBirth = new DateTime(1985,1,1);
+            customer.NationalityId = "1234567890";
+
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(customer);
+        }
+    }
+}
